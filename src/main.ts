111103d5813f8aca81fd88b5a91694d984023e13
Hanif Carroll <hanifcarroll@gmail.com>
2024-08-05 19:41:53 -0300

    Add swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('Gym Management Server')
+    .setDescription('Server for the gym management application suite.')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3001);
 }
+
 bootstrap();
